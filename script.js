771c810e7a17177c28ff34ac9ba7e6e9ea72a56a
Kristin <89033364+kriscodesstuff@users.noreply.github.com>
2023-06-30 23:35:31 -0500

    Add score variables
    Added the ability to keep score of all the matches

--- script.js
+++ script.js
@@ -11,38 +11,59 @@ function getComputerChoice() {
 
 const computerSelection = getComputerChoice();
 const playerSelection = prompt('Rock, Paper or Scissors');
-
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection,computerSelection){
     let lowercase = playerSelection.toLowerCase();
     console.log(lowercase);
-    
+     
     if(lowercase === computerSelection){
         console.log('Tie!');
     }else if((lowercase === 'rock' && computerSelection === 'paper') || 
     (lowercase === 'scissors' && computerSelection === 'rock') || (lowercase === 'paper' && computerSelection === 'scissors')){
         console.log(`You Lose! ${computerSelection} beats ${lowercase}`);
+        computerScore += 1
     }else{
         console.log(`You Win! ${lowercase} beats ${computerSelection}`);
+        playerScore += 1
     }
+
+    console.log(playerScore,computerScore);
 }
 
 
 
 function game(){
   playRound(playerSelection,computerSelection);
+
   let newPlayerSelection = prompt('Rock, Paper or Scissors?');
   let newComputerSelection = getComputerChoice();
+
   playRound(newPlayerSelection,newComputerSelection);
+
   newPlayerSelection = prompt('Rock, Paper or Scissors?');
   newComputerSelection = getComputerChoice();
+
   playRound(newPlayerSelection,newComputerSelection);
+
   newPlayerSelection = prompt('Rock, Paper or Scissors?');
   newComputerSelection = getComputerChoice();
+
   playRound(newPlayerSelection,newComputerSelection);
+
   newPlayerSelection = prompt('Rock, Paper or Scissors?');
   newComputerSelection = getComputerChoice();
+
   playRound(newPlayerSelection,newComputerSelection);  
+
+  if(computerScore > playerScore){
+    console.log('Computer wins!');
+  }else if(playerScore > computerScore){
+    console.log('Player wins!');
+  }else{
+    console.log("It's a tie!");
+  }
 }
 
 game();
